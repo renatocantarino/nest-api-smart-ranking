f37a2ebbce85a2544f6be6b48afba9b36286049f
Renato Cantarino <cast.carlosc.utic@sebrae.com.br>
2021-03-16 21:57:32 -0300

    evolucao

--- src/jogador/jogador.controller.ts
+++ src/jogador/jogador.controller.ts
@@ -1,31 +1,33 @@
-import { Body, Controller, Delete, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Post, Query } from '@nestjs/common';
 import { CreateDTO } from './dtos/create.dto'
 import { JogadorService } from './jogador.service'
 
 @Controller('api/v1/jogador')
 export class JogadorController {
 
-
     constructor(private readonly jogadorService: JogadorService) { }
 
-
-
     @Get()
-    async Obter() {
-       return this.jogadorService.GetAll();
+    async All() {
+        return this.jogadorService.GetAll();
     }
 
+    @Get("byEmail")
+    async byEmail(@Query('email') email: string) {
+        return this.jogadorService.ByEmail(email);
+    }
+
+
+
     @Post()
-    async Create(@Body() player: CreateDTO) {
+    async Create(@Body() player: CreateDTO) {        
         this.jogadorService.Create(player);
     }
 
 
     @Delete()
-    async Remove() {
-        return await JSON.stringify({
-            "nome": "NestJS"
-        })
+    async Remove(@Query('email') email: string) {
+        return this.jogadorService.Delete(email);
     }
 
 
--- src/jogador/jogador.service.ts
+++ src/jogador/jogador.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { CreateDTO } from './dtos/create.dto';
 import { Jogador } from './interfaces/jogador.interface';
 import { v4 as uuidv4 } from 'uuid';
@@ -37,17 +37,23 @@ export class JogadorService {
     }
 
 
+    async ByEmail(email: string): Promise<Jogador> {
+        const player = await this.jogadores.find(x => x.email === email);
+        if (!player)
+            throw new NotFoundException("Not found")
 
+        return player
+    }
 
     async GetAll(): Promise<Jogador[]> {
-        this.logger.warn("GET")
-
         return await this.jogadores;
     }
 
-    async GetbyEmail(): Promise<Jogador[]> {
-        this.logger.warn("GET")
+    async Delete(email: string): Promise<void> {
+        const player = await this.jogadores.findIndex(x => x.email === email);
+        if (player < 0)
+            throw new NotFoundException("Not found")
 
-        return await this.jogadores;
+        this.jogadores.splice(player, 1)
     }
 }
